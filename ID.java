44ab3d7347822b96dc12a8abf37eb37a0d19be87
LatifAutomation <69967585+LatifAutomation@users.noreply.github.com>
2022-06-13 21:09:32 +0530

    Add files via upload
    ID Locators

---
+++ ID.java
@@ -0,0 +1,31 @@
+package Locators;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ID {
+
+	public static void main(String[] args)
+	{
+		
+		ChromeDriver driver=new ChromeDriver();
+		
+		driver.get("file:///D:/HTML30May2022/ID.html");
+		
+		driver.manage().window().maximize();
+		
+		//First Name
+		
+		driver.findElement(By.tagName("input")).sendKeys("Maruti");
+		
+		// Middle Name
+		
+		driver.findElement(By.id("A")).sendKeys("Suzuki");
+		
+		//Last Name
+		
+		driver.findElement(By.id("B")).sendKeys("Car");
+		
+	}
+
+}
